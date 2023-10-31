47de6acf14acd83f353a0b7543ce2757318ef375
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-10-31 12:02:50 -0700

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,22 +8,29 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t)), *temp;
 
-	if (parent == NULL)
-		return (NULL);
 	if (new_node == NULL)
 		return (NULL);
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-
+	if (parent == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	if (parent->left != NULL)
 	{
-		new_node->left = parent->left;
-		parent->left->parent = new_node;
+		temp = parent->left;
+		parent->left = new_node;
+		new_node->left = temp;
+		temp->parent = new_node;
 	}
-	parent->left = new_node;
+	else
+	{
+		parent->left = new_node;
+		new_node->left = NULL;
+	}
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->right = NULL;
 	return (new_node);
 }
